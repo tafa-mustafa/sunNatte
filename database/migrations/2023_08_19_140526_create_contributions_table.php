2feaed577147e7b4cbff1d89c33077391c99fe5b
tafa-mustafa <tenetafa@gmail.com>
2025-07-04 15:49:17 +0000

    add colum date_contribition

--- database/migrations/2023_08_19_140526_create_contributions_table.php
+++ database/migrations/2023_08_19_140526_create_contributions_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('contributions', function (Blueprint $table) {
             $table->id();
             $table->string('montant');
-            $table->string('date_contribution');
             $table->string('transaction_id')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('tontine_id');
